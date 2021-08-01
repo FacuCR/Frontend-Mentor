b912638e28db1b1e9ce4c623f6da95917da60c67
FacuCR <facucastro302@gmail.com>
2021-07-31 23:43:02 -0300

    Calculos de la propina

--- Calculadora de Propinas/js/calculador.js
+++ Calculadora de Propinas/js/calculador.js
@@ -4,6 +4,8 @@
 const btnReset = document.querySelector(".btnReset");
 const inpNumPep = document.querySelector(".inpNumPep");
 const inpBill = document.querySelector(".inpBill");
+let tipAmount = document.querySelector(".tipAmount");
+let total = document.querySelector(".total");
 
 // Funcion para chequear que los inputs no esten vacios y habilitar el boton de reset
 const ComprobarInputs = () => {   
@@ -17,3 +19,19 @@ const ComprobarInputs = () => {
 inpBill.addEventListener("input", ComprobarInputs);
 
 inpNumPep.addEventListener("input", ComprobarInputs);
+
+// Calculadora de la propina por personas
+const CalcularPropina = () => {
+    if (inpBill.value !== "" && inpNumPep.value !== "") {
+        // Calculo de la propina que debe pagar cada persona
+        let porcentaje = document.querySelector('input[name="btnradio"]:checked').value;
+        let propina = parseFloat(inpBill.value) * porcentaje/100;
+        tipAmount.innerHTML = propina;
+
+        // Total a pagar
+        total.innerHTML = parseFloat(inpBill.value) + (propina * parseInt(inpNumPep.value));
+    } 
+}
+
+inpBill.addEventListener("input", CalcularPropina);
+inpNumPep.addEventListener("input", CalcularPropina);
